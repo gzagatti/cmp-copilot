bad3212a9156a511a86321dd77068e7cdceeab2a
gzagatti <gzagatti@users.noreply.github.com>
2023-10-31 16:32:14 +0800

    better label formatting and check whether source available.

--- lua/cmp_copilot/init.lua
+++ lua/cmp_copilot/init.lua
@@ -10,14 +10,41 @@ source.get_keyword_pattern = function()
   return '.'
 end
 
+source.is_available = function()
+  return vim.g.loaded_copilot == 1 and vim.b.copi_enabled ~= false
+end
+
+source.deindent = function(_, text)
+  local indent = string.match(text, '^%s*')
+  if not indent then
+    return text
+  end
+  return string.gsub(string.gsub(text, '^' .. indent, ''), '\n' .. indent, '\n')
+end
+
+source.label = function(_, text)
+  local shorten = function (str)
+    local short_prefix = string.sub(str, 0, 20)
+    local short_suffix = string.sub(str, string.len(str)-15, string.len(str))
+    local delimiter = " ... "
+    return short_prefix .. delimiter .. short_suffix
+  end
+  text = source:deindent(text)
+  text = text:gsub("^%s*", "")
+  return string.len(text) > 40 and shorten(text) or text
+end
+
 source.complete = function(self, params, callback)
   vim.fn['copilot#Complete'](function(result)
     callback({
       isIncomplete = true,
       items = vim.tbl_map(function(item)
-        local prefix = string.sub(params.context.cursor_before_line, item.range.start.character + 1, item.position.character)
         return {
-          label = prefix .. item.displayText,
+          label = source:label(item.text),
+          cmp = {
+            kind_hl_group = "CmpItemKindCopilot",
+            kind_text = "Copilot",
+          },
           textEdit = {
             range = item.range,
             newText = item.text,
@@ -41,13 +68,6 @@ source.complete = function(self, params, callback)
   end)
 end
 
-source.deindent = function(_, text)
-  local indent = string.match(text, '^%s*')
-  if not indent then
-    return text
-  end
-  return string.gsub(string.gsub(text, '^' .. indent, ''), '\n' .. indent, '\n')
-end
 
 return source
 
